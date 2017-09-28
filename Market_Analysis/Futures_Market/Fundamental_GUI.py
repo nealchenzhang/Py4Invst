14299413ca171eae5b429f7e25e5ed65f53429a7
nealchenzhang <nealzc1991@gmail.com>
2017-09-28 16:39:22 +0800

    New version with DataBase handling

--- Market_Analysis/Futures_Market/Fundamental_GUI.py
+++ Market_Analysis/Futures_Market/Fundamental_GUI.py
@@ -17,22 +17,108 @@
 
 __author__ = 'NealChenZhang'
 
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-
 import sys
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
 
 import matplotlib
 matplotlib.use("Qt5Agg")
 
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 import matplotlib.pyplot as plt
-import warnings
+from matplotlib.pylab import date2num
+import mpl_finance as mpf
 import pylab as pl
 
 pl.mpl.rcParams['font.sans-serif'] = ['SimHei']
 pl.mpl.rcParams['axes.unicode_minus'] = False
 
+import pandas as pd
+import datetime as dt
+
+from Data import *
+
+class MyMplCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100, *args, **kwargs):
+
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+
+        self.initial_figure()
+
+        super(MyMplCanvas, self).__init__(fig, *args, **kwargs)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def initial_figure(self):
+        pass
+
+class MyCandleStick(MyMplCanvas):
+    tick = '600614'
+    dbname = 'Stocks_Data'
+    start_date = '2017-01-03'
+    end_date = '2017-09-26'
+
+    def __init__(self, *args, **kwargs):
+        super(MyCandleStick, self).__init__(*args, **kwargs)
+
+    def initial_figure(self):
+
+        DB = MongoDB.MongoDBData()
+        df = DB.datafromMongoDB(self.dbname, self.tick)
+        df = df.loc[self.start_date:self.end_date, ['open', 'high', 'low', 'close']]
+
+        Data_list = []
+        for date, row in df.iterrows():
+            Date = date2num(dt.datetime.strptime(date, "%Y-%m-%d"))
+            Open, High, Low, Close = row[:4]
+            Data = (Date, Open, High, Low, Close)
+            Data_list.append(Data)
+
+        self.axes.xaxis_date()
+        # self.axes.plot(df['open'], df['close'])
+        # fig, ax = plt.subplots()
+        # fig.subplots_adjust(bottom=0.2)
+        # 设置X轴刻度为日期时间
+        # ax.xaxis_date()
+        # plt.xticks(rotation=45)
+        # plt.yticks()
+        # plt.title("股票代码：601558两年K线图")
+        # plt.xlabel("时间")
+        # plt.ylabel("股价（元）")
+        mpf.candlestick_ohlc(self.axes, Data_list, width=1.5, colorup='r', colordown='green')
+        # plt.grid()
+
+
+class MyMainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MyMainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Plot")
+
+        self.main_widget = QWidget(self)
+
+        gridlayout = QGridLayout(self.main_widget)
+        gridlayout.addWidget(MyCandleStick(self.main_widget, width=5, height=4, dpi=100))
+        gridlayout.addWidget(QLabel("Hello"))
+
+        self.setCentralWidget(self.main_widget)
+
+
+app = QApplication(sys.argv)
+
+window = MyMainWindow()
+window.show() # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec_()
--- test.py
+++ test.py
@@ -38,6 +38,14 @@
 
 # x = R
 
+import numpy as np
+import pandas as pd
+import datetime as dt
+import mpl_finance as mpf
+from matplotlib.pylab import date2num
+import matplotlib.pyplot as plt
+
+
 from Data import MongoDB
 stocklist = ['600614', '601118']
 DB = MongoDB.MongoDBData()
@@ -45,4 +53,24 @@ dbnames = ['Stocks_Data', 'Futures_Data']
 for stock in stocklist:
     DB.data2MongoDB('Stocks_Data', stock)
 
-df1 = DB.datafromMongoDB('Stocks_Data', '600614')
+df = DB.datafromMongoDB('Stocks_Data', '600614')
+df = df.loc[:, ['open', 'high', 'low', 'close']]
+
+Data_list = []
+for date, row in df.iterrows():
+    Date = date2num(dt.datetime.strptime(date, "%Y-%m-%d"))
+    Open, High, Low, Close = row[:4]
+    Data = (Date, Open, High, Low, Close)
+    Data_list.append(Data)
+
+fig, ax = plt.subplots()
+fig.subplots_adjust(bottom=0.2)
+# 设置X轴刻度为日期时间
+ax.xaxis_date()
+plt.xticks(rotation=45)
+plt.yticks()
+plt.title("股票代码：601558两年K线图")
+plt.xlabel("时间")
+plt.ylabel("股价（元）")
+mpf.candlestick_ohlc(ax,Data_list,width=1.5,colorup='r',colordown='green')
+plt.grid()
