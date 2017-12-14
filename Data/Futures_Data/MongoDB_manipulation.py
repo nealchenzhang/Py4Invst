f379444f9c3c5e2dee054a7d9d960b13707910de
nealchenzhang <nealzc1991@gmail.com>
2017-12-14 17:37:03 +0800

    Stocks Pairs Trading

--- Data/Futures_Data/MongoDB_manipulation.py
+++ Data/Futures_Data/MongoDB_manipulation.py
@@ -22,49 +22,64 @@ for i in collections:
 jiao = 'j1709'
 
 test_Coll = connection['Futures_Data'][jiao]
-# .find({'date':"2017-08-11"})
-dtstart = '13:30:00.0'
-dtend = '13:40:00.0'
 
 for index in test_Coll.index_information():
     pprint.pprint(index)
 
 if __name__ == '__main__':
     connection = pymongo.MongoClient('localhost', 27017)
-    coll = connection['test']['j1801']
+    coll = connection['test']['i1801']
 
 
     timedelta_1s = pd.Timedelta(seconds=1)
     timedelta_1m = pd.Timedelta(minutes=1)
     timedelta_15m = pd.Timedelta(minutes=15)
-    start = datetime.datetime(2017, 8, 10, 1, 40, 49, 000)
+
+    start = datetime.datetime(2017, 11, 13, 9, 00, 00, 000) - pd.Timedelta(hours=8)
+    end = datetime.datetime(2017, 11, 13, 15, 00, 00, 000)- pd.Timedelta(hours=8)
+    start_night = datetime.datetime(2017, 11, 10, 21, 00, 00, 000)
+    end_night = datetime.datetime(2017, 11, 10, 23, 30, 00, 000)
 
     coll.create_index([('datetime', pymongo.ASCENDING)])
 
-    for i in coll.find({'datetime': {'$gte': start,'$lte': start+timedelta_1s}}):
-        pprint.pprint(i)
+    cursor_i1801 = coll.find({'datetime': {'$gte': start, '$lte': end}})
+    df_i1801 = pd.DataFrame(list(cursor_i1801))
+    df_i1801.set_index('datetime', inplace=True)
+    df_i1801.drop(['exchange', 'time', '_id'], axis=1, inplace=True)
 
-    query = [
-        {
-            '$limit':100
-        },
-        {
-            '$project': {
-                'High': { $max: "lastPrice"},
-                'Low': { $min: "lastPrice"},
-                'Open': {'datetime': {'$gte': start, '$lte': start + timedelta_1s}}
-            }
-        }
-    ]
-        'datetime': {'$gte': start, '$lte': start + timedelta_1m}
+    ohlc_dict = {
+        'Open': 'first',
+        'High': 'max',
+        'Low': 'min',
+        'Close': 'last',
+        'Volume': 'last'
     }
 
-    projection = {'lastPrice':}
+    day1_trade_time_index = pd.date_range(start=start+pd.Timedelta(hours=8),
+                                          end=start+pd.Timedelta(hours=8)+pd.Timedelta(hours=1, minutes=14), freq='1min')
+    day2_trade_time_index = pd.date_range(start=start+pd.Timedelta(hours=8)+pd.Timedelta(hours=1, minutes=30),
+                                          end=start+pd.Timedelta(hours=8)+pd.Timedelta(hours=2, minutes=29), freq='1min')
+    day3_trade_time_index = pd.date_range(start=start+pd.Timedelta(hours=8)+pd.Timedelta(hours=4, minutes=30),
+                                          end=start+pd.Timedelta(hours=8)+pd.Timedelta(hours=5, minutes=59), freq='1min')
 
+    day_trade_time_index = day1_trade_time_index.append(day2_trade_time_index).append(day3_trade_time_index)
+    df_1m_day = pd.DataFrame(index=day_trade_time_index)
+    for i in day_trade_time_index:
+        tmp = df_i1801.loc[i-pd.Timedelta(hours=8): i-pd.Timedelta(hours=8) + pd.Timedelta(minutes=1) - pd.Timedelta(milliseconds=1),['lastPrice', 'openInterest', 'volume']]
+        df_1m_day.loc[i, 'Open'] = tmp.iloc[0]['lastPrice']
+        df_1m_day.loc[i, 'Close'] = tmp.iloc[-1]['lastPrice']
+        df_1m_day.loc[i, 'High'] = tmp['lastPrice'].max()
+        df_1m_day.loc[i, 'Low'] = tmp['lastPrice'].min()
+        df_1m_day.loc[i, 'Volume'] = tmp.iloc[-1]['volume']
+        df_1m_day.loc[i, 'OI'] = tmp.iloc[-1]['openInterest']
 
+    ohlc_dict = {
+        'Open': 'first',
+        'High': 'max',
+        'Low': 'min',
+        'Close': 'last',
+        'Volume': 'last',
+        'OI': 'last'
+    }
 
-
-
-for i in coll.find({'date':'2017-09-25'}):
-    pprint.pprint(i)
-    break
+    df_15m_day = (df_1m_day.resample('15T', closed='left', label='left').apply(ohlc_dict)).dropna()
